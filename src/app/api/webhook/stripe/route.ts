1af86e5a32bee2d811309712a32bff95ef8094df
judyta wlodarska <jdtwlodar@gmail.com>
2024-03-16 22:13:39 +0100

    #section 4 cart stripe fix

--- src/app/api/webhook/stripe/route.ts
+++ src/app/api/webhook/stripe/route.ts
@@ -1,18 +1,37 @@
-export async function POST(request: NextRequest): Promise<Response> {
-	const body: unknown = await request.json();
+import { type NextRequest } from "next/server";
+import Stripe from "stripe";
 
-	if (
-		typeof body === "object" &&
-		body &&
-		"productId" in body &&
-		typeof body.productId === "string"
-	) {
-		console.log(`Revalidating /product/${body.productId}`);
-		revalidatePath(`/product/${body.productId}`);
-		console.log(`Revalidating /products`);
-		revalidatePath(`/products`);
-		return new Response(null, { status: 204 });
-	} else {
-		return new Response(null, { status: 400 });
+export async function POST(request: NextRequest): Promise<Response> {
+	if (!process.env.STRIPE_SECRET_KEY) {
+		throw new Error("Missing STRIPE_SECRET_KEY");
 	}
+
+	if (!process.env.STRIPE_WEBHOOK_SECRET) {
+		throw new Error("Missing STRIPE_WEBHOOK_SECRET");
+	}
+
+	const stripe = new Stripe(process.env.STRIPE_WEBHOOK_SECRET, {
+		apiVersion: "2023-10-16",
+		typescript: true,
+	});
+
+	const signature = request.headers.get("stripe-signature");
+
+	if (!signature) {
+		return new Response("No signature", { status: 400 });
+	}
+
+	const event = stripe.webhooks.constructEvent(
+		await request.text(),
+		signature,
+		process.env.STRIPE_WEBHOOK_SECRET,
+	);
+
+	switch (event.type) {
+		case "checkout.session.completed": {
+			event.data.object.metadata?.cartId;
+			//console.dir(event, { depth: 999 });
+		}
+	}
+	return new Response(null, { status: 200 });
 }
