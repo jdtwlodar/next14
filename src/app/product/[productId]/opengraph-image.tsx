1abf06b402646f766d20b32ec2de7d178555467f
judyta wlodarska <jdtwlodar@gmail.com>
2024-03-17 19:39:19 +0100

    #section 4 pages opengraph 2

--- src/app/product/[productId]/opengraph-image.tsx
+++ src/app/product/[productId]/opengraph-image.tsx
@@ -1,14 +1,8 @@
 import { ImageResponse } from "next/og";
 import { getProductById } from "@/api/products";
 
-// export const runtime = "edge";
 export const contentType = "image/png";
-export const alt = "Open Graph Image";
-
-export const size = {
-	width: 1035,
-	height: 543,
-};
+export const alt = "Product Super shop Image";
 
 export default async function OpenGraphImageForSingleProduct({
 	params,
@@ -24,14 +18,7 @@ export default async function OpenGraphImageForSingleProduct({
 		(
 			<div tw="flex bg-white w-full h-full justify-items-center items-center">
 				<div tw="flex items-center justify-center border-gray-300 border-5  w-1/2">
-					{product.images[0] && (
-						<img
-							src={product.images[0]?.url}
-							alt={product.images[0].alt}
-							height={200}
-							width={200}
-						/>
-					)}
+					{product.images[0] && <img src={product.images[0]?.url} height={200} width={200} />}
 				</div>
 
 				<h2 tw="flex flex-col text-gray-900 text-left pr-5">
